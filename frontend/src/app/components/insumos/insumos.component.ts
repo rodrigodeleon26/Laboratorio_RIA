e17e01d97708866ae1cf3aeac2d6a21f62d5b670
Rodrigo de León <rodrigo.deleon@estudiantes.utec.edu.uy>
2024-06-18 08:41:12 -0300

    Filtrado de insumos

--- frontend/src/app/components/insumos/insumos.component.ts
+++ frontend/src/app/components/insumos/insumos.component.ts
@@ -5,36 +5,45 @@ import { startWith, map } from 'rxjs/operators';
 import { Insumo } from '../../models/insumo';
 import { InsumosService } from '../../services/insumos/insumos.service';
 
-
 @Component({
   selector: 'app-insumos',
   templateUrl: './insumos.component.html',
   styleUrls: ['./insumos.component.scss']
 })
 export class InsumosComponent implements OnInit {
-  insumos: Insumo[] = [];
+  insumos$: Observable<Insumo[]> | undefined;
   filter = new FormControl('');
-  filteredInsumos$: Observable<Insumo[]> | undefined;
 
   constructor(private insumosService: InsumosService) {}
 
   ngOnInit(): void {
-    this.insumosService.getInsumos().subscribe((data: Insumo[]) => {
-      this.insumos = data;
+    this.loadInsumos();
+    this.setupFilter();
+  }
+
+  loadInsumos() {
+    this.insumos$ = this.insumosService.getInsumos();
+  }
+
+  setupFilter() {
+    this.filter.valueChanges.pipe(
+      startWith('')
+    ).subscribe(() => {
+      const filterValue = this.filter.value;
+      this.insumos$ = this.filterInsumos(filterValue ? filterValue : '');
     });
+  }
 
-    this.filteredInsumos$ = this.filter.valueChanges.pipe(
-      startWith(''),
-      map((text) => this.search(text || '', this.insumos)) 
+  filterInsumos(text: string): Observable<Insumo[]> {
+    return this.insumosService.getInsumos().pipe(
+      map(insumos => this.search(text, insumos))
     );
-
-    this.filter.setValue('');
   }
 
   search(text: string, insumos: Insumo[]): Insumo[] {
-    return insumos.filter(insumo => {
-      const term = text.toLowerCase();
-      return insumo.nombre.toLowerCase().includes(term)
-    });
+    const term = text.toLowerCase();
+    return insumos.filter(insumo =>
+      insumo.nombre.toLowerCase().includes(term)
+    );
   }
 }
